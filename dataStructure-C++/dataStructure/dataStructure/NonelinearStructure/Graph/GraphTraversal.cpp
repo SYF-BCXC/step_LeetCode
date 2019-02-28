46dce05cc89b3fe5a36e16bd76645946d297ff4a
TCY <tangcaiyuan@hust.edu.cn>
2019-02-28 23:02:56 +0800

    Revert "2/28"
    This reverts commit 26e4d3681eb45324da72535793dbc95a4be7e6f5.

--- dataStructure-C++/dataStructure/dataStructure/NonelinearStructure/Graph/GraphTraversal.cpp
+++ dataStructure-C++/dataStructure/dataStructure/NonelinearStructure/Graph/GraphTraversal.cpp
@@ -1,45 +1,35 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-#include <vector>
-#include <queue>
+#include<vector>
 
 struct GraphNode
 {
 	int val;
-	vector<GraphNode *> neighbours;
-	GraphNode(int x) : val(x) {}
+	vector<GraphNode*> neighbours;
+	GraphNode(int x) :val(x) {}
 };
 
-// 给定图中一个点，遍历能访问到的所有节点
-void BFS_graph(GraphNode *node, int visit[])
-{
-	// 广度优先，先访问能访问的所有点，再依次访问这些点能访问的其他点
-	queue<GraphNode *> q;
-	q.push(node);
-	vector<int> result;
-	// 队列不为空，查看是否已经访问，未访问，则添加到result。
+void BFS_graph(GraphNode* node,int visit[]) {
 
-	// 访问result，打印结果
 }
 
-void DFS_graph()
-{
+void DFS_graph() {
+
 }
-/*
-int main()
-{
-	//
-	//	Graph:
-	//	Label(0):2 4
-	//	Label(1):0 2
-	//	Label(2):3
-	//	Label(3):4
-	//	Label(4):3
-	//
+
+
+int main() {
+	/*
+		Graph:
+		Label(0):2 4
+		Label(1):0 2
+		Label(2):3
+		Label(3):4
+		Label(4):3
+	*/
 	const int MAX_N = 5;
-	GraphNode *graph[MAX_N];
-	for (int i = 0; i < MAX_N; i++)
-	{
+	GraphNode* graph[MAX_N];
+	for (int i = 0; i < MAX_N; i++) {
 		graph[i] = new GraphNode(i);
 	}
 	graph[0]->neighbours.push_back(graph[2]);
@@ -49,24 +39,25 @@ int main()
 	graph[2]->neighbours.push_back(graph[3]);
 	graph[3]->neighbours.push_back(graph[4]);
 	graph[4]->neighbours.push_back(graph[3]);
-	// 打印图的结构
+	// 打印存储状态
 	printf("Graph:\n");
 	for (int i = 0; i < MAX_N; i++)
 	{
 		printf("Label(%d):", i);
-		for (int j = 0; j < graph[i]->neighbours.size(); j++)
-		{
+		for (int j = 0; j < graph[i]->neighbours.size(); j++) {
 			printf("%d ", graph[i]->neighbours[j]->val);
 		}
 		printf("\n");
 	}
-	// 深度优先
-	cout << "深度优先:\n";
+	// 广度优先遍历
+	cout << "广度优先遍历:\n";
 
-	// 广度优先
-	cout << "广度优先:\n";
 
-	// 释放内存
+	// 深度优先遍历
+	cout << "深度优先遍历:\n";
+
+
+	// 删除等操作
 	for (int i = 0; i < MAX_N; i++)
 	{
 		delete graph[i];
@@ -74,4 +65,3 @@ int main()
 	system("pause");
 	return 0;
 }
-*/
